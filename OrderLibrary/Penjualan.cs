36df57361bc03e2366ae583e4a0c7ef56bc6fc6b
calvino <calvino00@yahoo.com>
2018-07-23 14:14:22 +0700

    Add model

---
+++ OrderLibrary/Penjualan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_E_Commerce
+{
+     public class Penjualan
+    {
+        public string NoOrder { get; set; }
+        public DateTime Tanggal { get; set; }
+        public Akun DataAkun { get; set; }
+        public Barang DataBarang { get; set; }
+        public string Quantity { get; set; }
+        public decimal Total { get; set; }
+    }
+}
