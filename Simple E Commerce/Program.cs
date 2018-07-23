43680f28c1b2d0d8cc8f9e8d3d6bc345468c63a1
Griselda Guinarto <griseldaguinarto1998@gmail.com>
2018-07-23 15:21:59 +0700

    Making Interface

--- Simple E Commerce/Program.cs
+++ Simple E Commerce/Program.cs
@@ -16,7 +16,7 @@ namespace Simple_E_Commerce
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmAdminInterface());
         }
     }
 }
